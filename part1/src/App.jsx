62b75c9ec294b991aa88f19d831834833c9ec105
Michael Su <michaelsu.jobs@gmail.com>
2023-12-01 14:43:25 -0800

    Separate code logic into reusable components

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,19 +1,23 @@
+import Header from "./Header";
+import Total from "./Total";
+import Content from "./Content";
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
-  const exercises1 = 10;
+  const exercise1 = 10;
   const part2 = "Using props to pass data";
-  const exercises2 = 7;
+  const exercise2 = 7;
   const part3 = "State of a component";
-  const exercises3 = 14;
+  const exercise3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercise1}/>
+      <Content part={part2} exercise={exercise2}/>
+      <Content part={part3} exercise={exercise3}/>
+      <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3}/>
     </div>
   )
 }
